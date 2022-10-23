37aa40a8ae2962d1b8e452bfd24ede07f6edd570
jaykhan-dev <jaykhan.sound@gmail.com>
2022-10-23 18:44:01 -0400

    added eslint and prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from "react";
+// import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App grid place-items-center h-screen">
       <div>
         <h1>ShayBeas - Remixed with React</h1>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
