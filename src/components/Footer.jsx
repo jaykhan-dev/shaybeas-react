69896bc6365335cc6ac0a9fb0a892025be4b356e
jaykhan-dev <jaykhan.sound@gmail.com>
2022-10-23 21:25:33 -0400

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,53 @@
 const Footer = () => {
   return (
-    <footer className="text-white" id="footer-bg">
-      <div className="flex justify-center bg-black/80 py-20">
+    <footer className="text-white bg-fixed" id="footer-bg">
+      <div className="flex justify-center bg-black/50 py-20">
         <div className="lg:w-2/3">
-          <div className="bg-green-500 rounded-full px-4">
+          <div className="bg-gradient-to-r from-blue-500 to-green-500 rounded-lg px-4">
             <button className="bg-black px-4 uppercase">Contact Us</button>
           </div>
-          <div></div>
+          <div className="grid lg:grid-cols-3 gap-4 my-8">
+            <div className="border border-white/25 p-8 rounded-lg backdrop-blur-lg">
+              <h1 className="lg:text-6xl font-bold">First Contact</h1>
+              <p className="my-4">
+                ShayBeas Enterprises LLC <br />
+                Mint Julep Dr <br />
+                Riverview, FL, 33575
+              </p>
+              <a href="" className="text-blue-500 font-bold">
+                <p className="my-8">shaybeasenterprises@gmail.com</p>
+              </a>
+            </div>
+            <div className="backdrop-blur-lg col-span-2 border border-white/25 p-4 rounded-lg grid place-items-center">
+              <form action="" className="grid grid-cols-2 gap-4">
+                <div className="space-y-2 flex flex-col">
+                  <input
+                    placeholder="Name"
+                    type="text"
+                    className="p-4 rounded-lg bg-white/0 text-white border border-white/25"
+                  />
+                  <input
+                    placeholder="Email"
+                    className="p-4 rounded-lg bg-white/0 text-white border border-white/25"
+                  />
+                  <input
+                    type="text"
+                    placeholder="Phone"
+                    className="p-4 rounded-lg bg-white/0 text-white border border-white/25"
+                  />
+                  <button className="border p-4 rounded-tr-[20px] rounded-bl-[20px]">
+                    Submit
+                  </button>
+                </div>
+                <div className="">
+                  <textarea
+                    placeholder="Tell us about your idea"
+                    className="w-full bg-white/0 border border-white/25 rounded-lg p-4 h-full"
+                  />
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
       <div className="flex justify-center py-4 bg-black px-4">
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,11 +2,14 @@ const Hero = () => {
   return (
     <section className="bg-fixed" id="hero-bg">
       <div className="grid place-items-center py-20 bg-black/80 hover:bg-black/70 duration-300 text-white">
-        <div className="py-20 ">
-          <h1 className="lg:text-9xl lg:w-2/3 font-bold my-4">
-            Boldly going beyond leads
+        <div className="py-20">
+          <h1 className="lg:text-8xl font-bold text-4xl">
+            ShayBeas <br />
+            Enterprises
           </h1>
-          <p className="my-8">And actually dealing with referrals</p>
+          <h2 className="lg:text-4xl lg:w-2/3 font-bold my-4">
+            Boldly going beyond leads and actually dealing with referrals
+          </h2>
         </div>
       </div>
     </section>
