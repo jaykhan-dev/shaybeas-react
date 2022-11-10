0d4a492f8b72787dad00ef4c7cef1fbf802b0eaf
jaykhan-dev <jaykhan.sound@gmail.com>
2022-11-10 10:42:21 -0500

    changing welcome component

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,7 +2,10 @@ const Welcome = () => {
   return (
     <section className="" id="welcome-bg">
       <div className="flex justify-center">
-        <div className="grid place-items-center lg:w-2/3"></div>
+        <div className="grid place-items-center lg:w-2/3">
+          {/* WHY US */}
+          <div className="grid lg:grid-cols-3"></div>
+        </div>
       </div>
     </section>
   )
